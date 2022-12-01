e004569965b01dcedbecc0ffc138520cd20c51c0
Ilango <rajagopalilango@gmail.com>
2022-12-01 16:37:21 +0530

    👌 IMPROVE: Update day 1 solution - make it cleaner by a small amount

--- dec1/src/main.rs
+++ dec1/src/main.rs
@@ -3,33 +3,27 @@ use std::fs::File;
 use std::io::{BufReader, BufRead, Lines};
 
 fn main() {
-    // let mut file = File::open("input.txt").expect("Failed to open input.txt");
-    // let mut data = String::new();
-    //
-    // file.read_to_string(&mut data).expect("error reading file");
-    //
-    // println!("data: {}", data);
-
     let lines = read_lines();
-    let mut elf_cal: Vec<u64> = Vec::new();
-    let mut total_cal: u64 = 0;
+    let mut all_elf_cal: Vec<u64> = Vec::new();
+    let mut elf_total_cal: u64 = 0;
 
     for line in lines {
         let val = line.unwrap();
         if val != "" {
             let num: u64 = val.parse().unwrap();
-            total_cal += num;
+            elf_total_cal += num;
         } else {
-            elf_cal.push(total_cal);
-            total_cal = 0;
+            all_elf_cal.push(elf_total_cal);
+            elf_total_cal = 0;
         }
     }
 
-    // let max = elf_cal.iter().max().unwrap();
-    // println!("{:?}", max);
+    let max = all_elf_cal.iter().max().unwrap();
+    println!("{:?}", max);
 
-    elf_cal.sort_by(|a, b| a.partial_cmp(b).unwrap());
-    println!("{:?}", elf_cal);
+    all_elf_cal.sort_by(|a, b| b.partial_cmp(a).unwrap());
+    let top3 = all_elf_cal[0] + all_elf_cal[1] + all_elf_cal[2];
+    println!("{:?}", top3);
 }
 
 fn read_lines() -> Lines<BufReader<File>> {
