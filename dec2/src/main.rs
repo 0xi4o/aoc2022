ba8c332ae1e8f0d149a21950c8f7ae777bf40d0e
Ilango <rajagopalilango@gmail.com>
2022-12-02 12:14:57 +0530

    👌 IMPROVE: Solve day 2 part 2

--- dec2/src/main.rs
+++ dec2/src/main.rs
@@ -1,21 +1,21 @@
 use std::fs::File;
 use std::io::{BufReader, BufRead, Lines};
 
-enum Result {
-    W,
-    L,
-    D
-}
-
 fn main() {
     let lines = read_lines();
     let mut total_score: u64 = 0;
     for line in lines {
         let val = line.unwrap();
         let mut parts = val.split_whitespace();
-        let (opp, res) = (parts.next().unwrap(), parts.next().unwrap());
-        let result = play_round(opp, res);
-        let score = cal_score(res, result);
+        let (opp, round_end) = (parts.next().unwrap(), parts.next().unwrap());
+        let result = match round_end {
+            "X" => "L",
+            "Y" => "D",
+            "Z" => "W",
+            _ => ""
+        };
+        let res = play_round(opp, &result);
+        let score = cal_score(res, &result);
         total_score += score;
     }
     println!("{}", total_score);
@@ -28,30 +28,30 @@ fn read_lines() -> Lines<BufReader<File>> {
     reader.lines()
 }
 
-fn play_round(opp: &str, res:&str) -> Result {
-    if opp == "A" && res == "X" {
-        Result::D
-    } else if opp == "A" && res == "Y" {
-        Result::W
-    } else if opp == "A" && res == "Z" {
-        Result::L
-    } else if opp == "B" && res == "X" {
-        Result::L
-    } else if opp == "B" && res == "Y" {
-        Result::D
-    } else if opp == "B" && res == "Z" {
-        Result::W
-    } else if opp == "C" && res == "X" {
-        Result::W
-    } else if opp == "C" && res == "Y" {
-        Result::L
+fn play_round<'a>(opp: &'a str, result: &'a str) -> &'a str {
+    if opp == "A" && result == "W" {
+        "Y"
+    } else if opp == "A" && result == "L" {
+        "Z"
+    } else if opp == "A" && result == "D" {
+        "X"
+    } else if opp == "B" && result == "W" {
+        "Z"
+    } else if opp == "B" && result == "L" {
+        "X"
+    } else if opp == "B" && result == "D" {
+        "Y"
+    } else if opp == "C" && result == "W" {
+        "X"
+    } else if opp == "C" && result == "L" {
+        "Y"
     } else {
-        Result::D
+        "Z"
     }
 
 }
 
-fn cal_score(res: &str, result: Result) -> u64 {
+fn cal_score(res: &str, result: &str) -> u64 {
     let res_score = match res {
         "X" => 1,
         "Y" => 2,
@@ -60,9 +60,10 @@ fn cal_score(res: &str, result: Result) -> u64 {
     };
 
     let result_score = match result {
-        Result::L => 0,
-        Result::D => 3,
-        Result::W => 6
+        "L" => 0,
+        "D" => 3,
+        "W" => 6,
+        _ => 0
     };
 
     res_score + result_score
